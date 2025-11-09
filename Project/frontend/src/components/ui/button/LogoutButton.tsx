2c4d794f1dbc284b865f1ad0bc71d7686b5b520c
tims-exe <timonkjohn@gmail.com>
2025-11-09 17:29:03 +0530

    frontend ui fixes

--- Project/frontend/src/components/ui/button/LogoutButton.tsx
+++ Project/frontend/src/components/ui/button/LogoutButton.tsx
@@ -6,7 +6,7 @@ export default function LogoutButton() {
     return (
         <button
             onClick={logout}
-            className="mt-6 bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600 transition-colors"
+            className="border-2 border-red-400 text-red-400 rounded hover:bg-red-400 hover:text-white hover:cursor-pointer transition-colors h-[40px] px-3"
         >
             Logout
         </button>
